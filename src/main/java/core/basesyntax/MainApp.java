0e6012da11c0808d64e6aaf68e41950643d295fe
Paweł Wojciechowski <p5w3l.kontakt@gmail.com>
2026-07-17 17:48:04 +0200

    ...

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,12 +1,12 @@
 package core.basesyntax;
-// Mate Academy
-public class MainApp {
 
+public class MainApp {
     public static void main(String[] args) {
         Machine truck = new Truck();
         Machine bulldozer = new Bulldozer();
         Machine excavator = new Excavator();
-        Machine[] machines = {truck, bulldozer, excavator};
+
+        Machine[] machines = new Machine[]{truck, bulldozer, excavator};
 
         for (Machine machine : machines) {
             machine.doWork();
